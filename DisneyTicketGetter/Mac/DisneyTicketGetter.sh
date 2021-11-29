07f6c209fb20957c7065757d600b96b7c9b00951
Kei-Kokubun <58175690+Kei-Kokubun@users.noreply.github.com>
2021-11-29 18:30:42 +0900

    Update DisneyTicketGetter.sh

--- DisneyTicketGetter/Mac/DisneyTicketGetter.sh
+++ DisneyTicketGetter/Mac/DisneyTicketGetter.sh
@@ -7,6 +7,11 @@ do
     open -ga Safari https://reserve.tokyodisneyresort.jp/ticket/search/  #Safariでディズニーチケット購入ページに接続
     sleep 0.4 #サイトに文章が表示されるまで0.4秒待機
     text=`osascript -e 'tell app "Safari" to get the text of the current tab of window 1'` #接続したサイトに表示される文章を取得
+    while [ -z "$text" ] #何の文章も取得できなかった場合に以下のdo~doneを繰り返す
+     do
+        sleep 0.1 #0.1秒待機
+        text=`osascript -e 'tell app "Safari" to get the text of the current tab of window 1'` #サイトの文章を再取得
+     done
     echo "$text" | grep -q "$text_check" #接続したサイトに表示される文章に"this website ~"が含まれるか調べる
     if [ $? -eq 0 ]  ; then #アクセス集中ページに接続したかの判別
         i=$(( i + 1 )) #(アクセス集中ページに接続した場合)今までのアクセス回数を1増やす
